e53669831655924404605367f6a4aee725fad694
SWU-ISBO-2024 <ShovelWareUYU@gmail.com>
2024-07-15 20:55:16 -0300

    Update register.php

--- Programación Web/Tienda/register.php
+++ Programación Web/Tienda/register.php
@@ -7,8 +7,6 @@ $username = $input['username'];
 $mail = $input['mail'];
 $password = $input['password'];
 
-// Aquí deberías realizar la lógica para registrar al usuario en la base de datos
-// Ejemplo simple sin base de datos
 
 // Validar que el usuario no exista ya (aquí deberías consultar tu base de datos)
 if ($username === 'existe' || $mail === 'existe@mail.com') {
