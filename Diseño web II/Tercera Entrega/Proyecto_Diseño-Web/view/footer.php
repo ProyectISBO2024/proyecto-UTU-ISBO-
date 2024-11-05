58a122005f8eb8d6d6108903017f89f6a25f0ce5
SWU-ISBO-2024 <ShovelWareUYU@gmail.com>
2024-11-05 18:16:56 -0300

    Delete Diseño web II directory

--- Diseño web II/Tercera Entrega/Proyecto_Diseño-Web/view/footer.php
+++
@@ -1,6 +0,0 @@
-</div> <!-- Cierre de contenido -->
-    <footer>
-        <p>&copy; 2024 Compramaster </p>
-    </footer>
-</body>
-</html>
--- Diseño web II/Tercera Entrega/Proyecto_Diseño-Web/view/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-
-// Redirigir a la página de inicio de sesión
-header("Location: login.php");
-exit();
